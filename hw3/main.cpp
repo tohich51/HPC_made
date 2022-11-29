d4a5b01fcde3486526e61c6b367360b67aacaa7d
tohich51 <tohich51@gmail.com>
2022-11-29 13:06:50 +0300

    Create main.cpp

---
+++ hw3/main.cpp
@@ -0,0 +1,30 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+    const size_t N = 100;
+    const size_t chunk = 3;
+
+    int i, tid;
+    float a[N], b[N], c[N];
+
+    for (i = 0; i < N; ++i)
+    {
+        a[i] = b[i] = (float)i;
+    }
+
+#pragma omp parallel shared(a,b,c) private(i,tid)
+    {
+        tid = omp_get_thread_num();
+#pragma omp for schedule(static, chunk)
+        for (i = 0; i < N; ++i)
+        {
+            c[i] = a[i] + b[i];
+            printf("tid = %d, c[%d] = %f\n", tid, i, c[i]);
+        }
+    } 
+
+    return 0;
+}
